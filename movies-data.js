3a5377a22019db7629a912eafe79439d76e3effa
Driendf <130811818+Driendf@users.noreply.github.com>
2024-12-23 23:59:48 -0300

    Add files via upload

---
+++ movies-data.js
@@ -0,0 +1,18 @@
+const additionalMovies = [
+  {
+    title: "Nuevo título 1",
+    image: "https://via.placeholder.com/150x225",
+    doblado: true,
+    link: "https://www.dramabreeze.site/2024/12/nuevo-titulo-1.html"
+  },
+  {
+    title: "Nuevo título 2",
+    image: "https://via.placeholder.com/150x225",
+    doblado: false,
+    link: "https://www.dramabreeze.site/2024/12/nuevo-titulo-2.html"
+  },
+  // Agrega más elementos aquí
+];
+
+// Mezclar con la lista existente
+movies.push(...additionalMovies);
