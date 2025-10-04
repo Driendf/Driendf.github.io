372fffa562bcb01fa4f0d74185b8de161d513050
Driendf <130811818+Driendf@users.noreply.github.com>
2025-10-04 19:37:19 -0300

    Add files via upload

---
+++ Pelicula Post Data.js
@@ -0,0 +1,9 @@
+window.dataCatalogo = window.dataCatalogo || [];
+
+window.dataCatalogo.push(
+
+{"title":"Malditas","img":"https://image.tmdb.org/t/p/w154/oxNqvKAFQNQbRPqLsP7zfdXLXYK.jpg","category":"peliculas","tags":["drama"],"sinopsis":"Las vidas de seis niñas pertenecen a mundos diferentes aunque conectados entre sí por una muñeca cuya existencia se remonta a 1976, cuando apareció entre los escombros del gran incendio del orfanato de San Carlos, al norte de España.","seasons":[{"episodes":[{"title":"Malditas Peli 1","link":"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"}]}]},
+
+
+);
+
---
+++ Serie Post Data.js
@@ -0,0 +1,41 @@
+window.dataCatalogo = window.dataCatalogo || [];
+window.dataCatalogo.push(
+{
+  "title":"Zorro",
+  "img":"https://image.tmdb.org/t/p/w154/8fofhCU3SRcRdXYL3psiW4zfyDt.jpg",
+  "category":"series",
+  "tags":["drama"],
+  "sinopsis":"Al convertirse en alcalde de Los Ángeles de 1821, Don Diego de la Vega se enfrenta a la resistencia del corrupto empresario Don Emmanuel.. زورو",
+  "seasons":[
+    {
+      "episodes":[
+        {
+          "title": "Zorro - Episodio 1",
+          "links": [
+            "https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html",
+			"https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html",
+            "https://driendf.github.io/T1 [075.075] (SubEsp) La Venganza del Zorro.html"
+          ]
+        },
+        {
+          "title": "Zorro - Episodio 2",
+          "links": [
+            "https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"
+          ]
+        }
+      ]
+    },
+    {
+      "episodes":[
+        {
+          "title": "Zorro - Episodio 1 T2",
+          "links": [
+            "https://driendf.github.io/T1 [074.074] (SubEsp) A Estrella para toda mi vida.html"
+          ]
+        }
+        // ... más episodios
+      ]
+    }
+  ]
+}
+);
